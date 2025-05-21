8aa6cdc7add144a81d5a1b3255104f0606ec70f6
MikoAlt <88568525+MikoAlt@users.noreply.github.com>
2025-05-21 12:11:32 +0700

    first commit

---
+++ include/main.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Miko on 5/21/2025.
+//
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_HPP
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Miko on 5/21/2025.
+//
+
+#include "main.hpp"
